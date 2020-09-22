77e845cd089e2339b6d55bdf90fb86d970cfc551
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2020-09-23 01:42:24 +0530

    Project with Basic Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { StoreComponent } from './components/store/store.component';
+import { FiltersComponent } from './components/store/filters/filters.component';
+import { ProductListComponent } from './components/store/product-list/product-list.component';
+import { CartComponent } from './components/store/cart/cart.component';
+import { ProductComponent } from './components/store/product-list/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    StoreComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
